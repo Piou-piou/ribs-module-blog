bb420952e94b88e14c6f867a3ce5902cdf50520e
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-02-07 16:07:49 +0100

    prepare controller for index

--- router/config.php
+++ router/config.php
@@ -1,3 +1,6 @@
 <?php
-	define('BATAILLEWEBROOT', str_replace("index.php", '', $_SERVER['SCRIPT_NAME'])."modules/blog/app/views/");
-	define('BATAILLEROOT', str_replace("index.php", '', $_SERVER['SCRIPT_FILENAME'])."modules/blog/app/views/");
+	//For the root folder of the blog
+	define("BLOGWEBROOT", str_replace("$page_root", '', $_SERVER['SCRIPT_NAME'])."modules/blog/app/views/");
+	
+	//For the root folder of the blog -> for include and require
+	define('BLOGROOT', str_replace("$page_root", '', $_SERVER['SCRIPT_FILENAME'])."modules/blog/app/views/");
--- router/routes.php
+++ router/routes.php
@@ -8,6 +8,10 @@
 			\core\HTML\flashmessage\FlashMessage::setFlash("Cette page n'existe pas ou plus");
 			header("location:".WEBROOT);
 		}
+		
+		if ($page == "index") {
+			$this->controller = "blog/app/controller/initialise/index.php";
+		}
 	}
 	else {
 		\core\HTML\flashmessage\FlashMessage::setFlash("L'accès à ce module n'est pas configurer ou ne fais pas partie de votre offre, contactez votre administrateur pour résoudre ce problème", "info");
