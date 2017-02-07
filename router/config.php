f19f6e77520ec663afc99a6422eeafdf0ee2018b
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-02-07 13:26:38 +0100

    prepare files to begin creation of this module

---
+++ router/config.php
@@ -0,0 +1,3 @@
+<?php
+	define('BATAILLEWEBROOT', str_replace("index.php", '', $_SERVER['SCRIPT_NAME'])."modules/blog/app/views/");
+	define('BATAILLEROOT', str_replace("index.php", '', $_SERVER['SCRIPT_FILENAME'])."modules/blog/app/views/");
