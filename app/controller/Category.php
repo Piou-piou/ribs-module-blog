ee7cb30ddc38139d3321ba57c994b44f66c09bb4
Piou-piou <pilloud.anthony@gmail.com>
2017-02-07 19:10:41 +0100

    add getting all categories of an article

--- app/controller/Category.php
+++ app/controller/Category.php
@@ -2,7 +2,9 @@
 	namespace modules\blog\app\controller;
 	
 	
-	class Categorie {
+	use core\App;
+	
+	class Category {
 		
 		
 		//-------------------------- BUILDER ----------------------------------------------------------------------------//
@@ -13,6 +15,34 @@
 		
 		
 		//-------------------------- GETTER ----------------------------------------------------------------------------//
+		/**
+		 * get all categories of an article
+		 */
+		public function getCategoryArticle() {
+			$dbc = App::getDb();
+			
+			$query = $dbc->select()
+				->from("_blog_article")
+				->from("_blog_category")
+				->from("_blog_article_category")
+				->where("_blog_article.ID_article", "=", Blog::$parametre_router, "OR")
+				->where("_blog_article.url", "=", Blog::$parametre_router, "AND")
+				->where("_blog_article_category.ID_article", "=", "_blog_article.ID_article", "AND", true)
+				->where("_blog_article_category.ID_category", "=", "_blog_category.ID_category", "", true)
+				->get();
+			
+			if ((is_array($query)) && (count($query) > 0)) {
+				$categories = [];
+				foreach ($query as $obj) {
+					$categories = [
+						"id_category" => $obj->ID_category,
+						"category" => $obj->category
+					];
+				}
+				
+				App::setValues(["categories" => $categories]);
+			}
+		}
 		//-------------------------- END GETTER ----------------------------------------------------------------------------//
 		
 		
--- app/controller/initialise/article.php
+++ app/controller/initialise/article.php
@@ -1,5 +1,12 @@
 <?php
-	$blog = new \modules\blog\app\controller\Article();
-	$blog->getArticle();
+	$article = new \modules\blog\app\controller\Article();
+	$article->getArticle();
+	
+	$category = new \modules\blog\app\controller\Category();
+	$category->getCategoryArticle();
 	
 	$arr = \core\App::getValues();
+	
+	echo("<pre>");
+	print_r($arr);
+	echo("</pre>");
