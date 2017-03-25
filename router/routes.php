ddfdff85d343b0415f1c3ea4e0cd7be8dd039e06
Unknown <pilloud.anthony@gmail.com>
2017-03-25 23:59:49 +0100

    change route file for new errors redirect

--- router/routes.php
+++ router/routes.php
@@ -7,8 +7,7 @@
 	
 	if (\core\modules\GestionModule::getModuleActiver("blog")) {
 		if (!in_array($this->page, $pages_blog)) {
-			\core\HTML\flashmessage\FlashMessage::setFlash("Cette page n'existe pas ou plus");
-			header("location:".WEBROOT);
+			header("location:".WEBROOT."404");
 		}
 		
 		if ($this->page == "index") {
@@ -26,6 +25,5 @@
 		}
 	}
 	else {
-		\core\HTML\flashmessage\FlashMessage::setFlash("L'accès à ce module n'est pas configurer ou ne fais pas partie de votre offre, contactez votre administrateur pour résoudre ce problème", "info");
-		header("location:".WEBROOT);
+		header("location:".WEBROOT."404");
 	}
